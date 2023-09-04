acd5b325e2febcdec38fdde8315ff0650db3c589
zeenathshaik <143864951+zeenathshaik@users.noreply.github.com>
2023-09-04 17:07:10 +0530

    Add files via upload

---
+++ ft.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA9LdjyD9qgGz7KjtImUHZrp4ap4HuNq0U",
+  authDomain: "multipages-6c6a0.firebaseapp.com",
+  databaseURL: "https://multipages-6c6a0-default-rtdb.firebaseio.com",
+  projectId: "multipages-6c6a0",
+  storageBucket: "multipages-6c6a0.appspot.com",
+  messagingSenderId: "1098866635386",
+  appId: "1:1098866635386:web:492a46273782242de841c3",
+  measurementId: "G-WK3T7V5ETK"
+};
